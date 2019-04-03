e54144a5fa665ebb0138ae4c85edea965def3cbb
meslex <ulimwegu@gmail.com>
2019-04-03 16:49:02 +0300

    Add project files.

---
+++ MoviesShop/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MoviesShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
